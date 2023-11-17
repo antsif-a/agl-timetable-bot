b292b0e554a3e7690a0138d3c2587a7d55f459fa
antsif-a <summet.dev@gmail.com>
2023-11-18 01:44:39 +0300

    fix(command): send /latest images as form-data

--- src/modules/bot/commands/timetable.ts
+++ src/modules/bot/commands/timetable.ts
@@ -1,6 +1,18 @@
 import { VKApi } from 'node-vk-sdk';
-import { Context } from 'grammy';
+import { Context, InputFile } from 'grammy';
 import { BotCommand } from '@/modules/bot/bot-command';
+import {
+    PhotosPhoto,
+    PhotosPhotoSizes,
+} from 'node-vk-sdk/distr/src/generated/Models';
+import axios from 'axios';
+import { Buffer } from 'node:buffer';
+
+function findLargestPhotoSize(photo: PhotosPhoto) {
+    return photo.sizes
+    .sort((a: PhotosPhotoSizes, b: PhotosPhotoSizes) => b.width - a.width)
+    .at(0)!;
+}
 
 export default class TimetableCommand implements BotCommand {
     command = 'latest';
@@ -23,15 +35,17 @@ export default class TimetableCommand implements BotCommand {
             return ctx.reply('Расписаний нет.');
         }
 
-        // todo add checks
+        for (const attachment of latestWall.attachments) {
+            const photoUrl = findLargestPhotoSize(attachment.photo).url;
+            const image = new InputFile(() =>
+                axios.get(photoUrl, {
+                    responseType: 'arraybuffer'
+                }).then((res) => res.data as Buffer));
+            await ctx.replyWithPhoto(image);
+        }
 
-        if (latestWall.attachments && latestWall.attachments[0].photo &&
-            latestWall.attachments[0].photo.sizes) {
-            const sizes = latestWall.attachments[0].photo.sizes;
-            const maxSize = sizes.sort((a, b) => a.width - b.width).
-                reverse().
-                at(0);
-            await ctx.replyWithPhoto(maxSize!.url!);
+        if (latestWall.text) {
+            await ctx.reply(latestWall.text);
         }
     }
 }
